42420fc8dbf5ab42c480f357bdce31b9d22fece4
rohitsingh7879 <rohitsingh998195@gmail.com>
2025-03-26 18:24:09 +0530

    Added: new code for Editor

--- app.js
+++ app.js
@@ -1,58 +1,36 @@
 require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const database = require("./config/database");
-const routes = require("./app/Http/routes");
+const express = require('express');
+const mongoose  = require('mongoose')
+const cors = require('cors');
+const database = require('./config/database');
+const routes = require('./app/Http/routes');
 
-const app = express();
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://16.170.107.234/dashboard1/",
-  "http://16.170.107.234/Holiday/",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, 
-  })
-);
+const app = express()
+app.use(cors({
+    origin: '*'
+}))
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended : true}))
 
-mongoose
-  .connect(database.mongodb.uri, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log("Mongodb is connected...");
-  })
-  .catch((err) => {
-    console.log("Mongodb is not connected...", err);
-  });
+mongoose.connect(database.mongodb.uri,{
+    useNewUrlParser : true
+}).then(()=>{
+    console.log("Mongodb is connected...")
+}).catch((err)=>{
+    console.log("Mongodb is not connected...",err)
+})
 
-app.use("/holiday/", routes);
-app.listen(process.env.PORT, () => {
-  console.log(`Server is started on Port ${process.env.PORT}`);
-});
 
-app.get("/holiday_dashboard_health", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Working", data: "", success: true, status: 200 });
+
+app.use('/holiday/',routes)
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is started on Port ${process.env.PORT}`);
+})
+
+app.get('/holiday_dashboard_health', (req, res) => {
+    res.status(200).json({ message: "Working", data: "" , success : true , status : 200 });
 });
 
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .json({ message: "Page not Found", data: "", success: true, status: 404 });
-});
+    res.status(404).json({ message: "Page not Found", data: "" , success : true , status : 404 });
+})
